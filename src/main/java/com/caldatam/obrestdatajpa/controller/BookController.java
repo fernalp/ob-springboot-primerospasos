0f5202f90378cb86001f11be432c779daaed26cd
fernalp <almanzafernando@hotmail.com>
2023-02-17 22:12:40 -0500

    Se modifica endpoint de crear libro agregando la protección de no crear un libro pasandole un id, se agrega endpoint de actualizar libro, se agrega endpoint de eliminar libro

--- src/main/java/com/caldatam/obrestdatajpa/controller/BookController.java
+++ src/main/java/com/caldatam/obrestdatajpa/controller/BookController.java
@@ -2,6 +2,8 @@ package com.caldatam.obrestdatajpa.controller;
 
 import com.caldatam.obrestdatajpa.entities.Book;
 import com.caldatam.obrestdatajpa.repository.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,8 @@ import java.util.Optional;
 
 @RestController
 public class BookController {
+
+    private final Logger log = LoggerFactory.getLogger(BookController.class);
 
     private final BookRepository bookRepository;
 
@@ -24,7 +28,7 @@ public class BookController {
     //Buscar todos los libros
 
     /**
-     * http://localhost:8080/api/books
+     *
      * @return List<Book>
      */
     @GetMapping("/api/books")
@@ -52,14 +56,54 @@ public class BookController {
 
     //Crear un nuevo libro en la base de datos
     @PostMapping("/api/books")
-    public Book create(@RequestBody Book book, @RequestHeader HttpHeaders headers){
+    public ResponseEntity<Book> create(@RequestBody Book book, @RequestHeader HttpHeaders headers){
         System.out.println(headers.get("User-Agent"));
-        return bookRepository.save(book);
+        if (book.getId() != null){
+            log.warn("Trying to create a book with id");
+            System.out.println("Trying to create a book with id");
+            return ResponseEntity.badRequest().build();
+        }
+
+        Book result = bookRepository.save(book);
+        return ResponseEntity.ok(result);
     }
 
     //Actualizar un libro en la base de datos
-
+    @PutMapping("/api/books")
+    public ResponseEntity<Book> update(@RequestBody Book book){
+        if (book.getId()==null){
+            log.warn("Trying to update a non existent book");
+            return ResponseEntity.badRequest().build();
+        }
+        if (!bookRepository.existsById(book.getId())){
+            log.warn("Trying to update a non existent book");
+            return ResponseEntity.notFound().build();
+        }
+        Book result = bookRepository.save(book);
+        return ResponseEntity.ok(result);
+    }
 
     //Borrar un libro de la base de datos
+
+    @DeleteMapping("/api/books/{id}")
+    public ResponseEntity<Book> delete(@PathVariable Long id){
+        if (!bookRepository.existsById(id)){
+            log.warn("Trying to delete a non existent book");
+            return ResponseEntity.notFound().build();
+        }
+        bookRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    //Borrar todos los libros
+    @DeleteMapping("/api/books")
+    public ResponseEntity<Book> deleteAll(){
+        if (bookRepository.count() > 0){
+            bookRepository.deleteAll();
+            log.info("REST request for delete all books");
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 
 }
